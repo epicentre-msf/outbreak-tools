9dfe44f89a4709bbdfc2027daaf1b30099708ef1
Yves Amevoin <amevoina@yahoo.fr>
2023-03-14 16:05:33 +0100

    updated designer in dev branch

--- Rscripts/preparing_folders.R
+++ Rscripts/preparing_folders.R
@@ -7,22 +7,22 @@ update_designer(update_stable = 1)
 # update by adding the stable
 #update_designer(update_stable = 1)
 
-# clean the output
-clear_output()
+# # clean the output
+# clear_output()
 
-# preparing the demo folder
+# # preparing the demo folder
 
-path_fake_dataset  <-  "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/LL-test-Marine_with fake data.xlsb" # nolint
-path_fake_geobase  <- "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/geobase_obt_yem_20230112.xlsx" # nolint
-setup_filename  <- "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/setup_measles_Yemen_20230201.xlsb" # nolint
-demo_folder  <- "./demo"
+# path_fake_dataset  <-  "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/LL-test-Marine_with fake data.xlsb" # nolint
+# path_fake_geobase  <- "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/geobase_obt_yem_20230112.xlsx" # nolint
+# setup_filename  <- "D:/MSF/OutbreakTools - Library/03 - Test/TEST MARINE/20230131/setup_measles_Yemen_20230201.xlsb" # nolint
+# demo_folder  <- "./demo"
 
-# preparing the demo folder
-clear_output("./demo")
+# # preparing the demo folder
+# clear_output("./demo")
 
-prepare_demo(fake_dataset = path_fake_dataset,
-             fake_geobase = path_fake_geobase,
-             setup_filename = setup_filename) # nolint
+# prepare_demo(fake_dataset = path_fake_dataset,
+#              fake_geobase = path_fake_geobase,
+#              setup_filename = setup_filename) # nolint
 
 
 # create a class
